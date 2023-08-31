7aadb4deb58701d9cf1f0d3229a3b527d8f43003
arcbsr <bsrsoftbd@gmail.com>
2023-08-31 19:31:09 +0100

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -8,6 +8,6 @@ GIT_REPO="https://github.com/your-username/your-repo.git"
 BRANCH="main"  # Change to your desired branch
 
 # SSH into the remote server
-ssh rafi@146.190.152.13 -i connect.rsa <<EOF
+ssh rafi@146.190.152.133 -i connect.rsa <<EOF
     echo 'hello world'
 EOF
