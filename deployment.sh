497ce9aa328b86011427215a40c6229a960d0b10
arcbsr <bsrsoftbd@gmail.com>
2023-08-31 17:49:00 +0100

    Create deployment.sh

---
+++ deployment.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Define variables
+REMOTE_USER="rafi"
+DROPLET_IP="146.190.152.133"
+PROJECT_PATH="/ProConnect/project"
+GIT_REPO="https://github.com/your-username/your-repo.git"
+BRANCH="main"  # Change to your desired branch
+
+# SSH into the remote server
+ssh "$REMOTE_USER@$DROPLET_IP -i ~/connect.rsa" <<EOF
+    cd "$PROJECT_PATH"
+    git pull
+EOF
